c51912911acc29084f73009b95ac911cd9d5e72f
F80 <43803343+f80dev@users.noreply.github.com>
2022-07-24 12:48:57 +0200

    preversion

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -7,6 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class SectionComponent implements OnInit {
   @Input() title: any;
+  @Input() bgcolor="white";
 
   constructor() { }
 
