2638e4f85e373778ba28178dba5850ac69195817
f80dev <herve.hoareau@f80.fr>
2022-09-01 12:30:01 +0200

    Version avec formulaire

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,112 @@
+import {AfterViewInit, Component, Input} from '@angular/core';
+declare var anime: any;
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements AfterViewInit {
+  title = 'NFluentWebSite';
+  name:any="";
+  message: any="";
+  subject="";
+  email="";
+
+  init(){
+    anime({
+      targets: '.small-round',
+      scale: {
+        value:1.6,
+        duration: 1,
+        endDelay: 1000,
+      },
+      translateX: {
+        value:60,
+        duration: 1,
+        endDelay: 1000,
+      },
+      direction: 'alternate',
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+
+    anime({
+      targets: '.medium-round',
+      scale: 0.5,
+      translateX: {
+        value:-150,
+        duration: 1,
+        endDelay: 1000,
+
+      },
+      direction: 'alternate',
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+
+    anime({
+      targets: '.rounds-logo',
+      rotate: {
+        value:360,
+        duration: 1,
+        delay:2000
+      },
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+    this.play();
+  }
+
+  play(){
+    anime({
+      targets: '.small-round',
+      scale: {
+        value:1.6,
+        duration: 2000,
+        endDelay: 1000,
+      },
+      translateX: {
+        value:60,
+        duration: 2000,
+        endDelay: 1000,
+      },
+      direction: 'alternate',
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+
+    anime({
+      targets: '.medium-round',
+      scale: {
+        value:0.5,
+        duration: 2000,
+        endDelay: 1000,
+      },
+      translateX: {
+        value:-150,
+        duration: 2000,
+        endDelay: 1000,
+
+      },
+      direction: 'alternate',
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+
+    anime({
+      targets: '.rounds-logo',
+      rotate: {
+        value:360,
+        duration: 4000,
+        delay:2000
+      },
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.play();
+  }
+
+  open_menu(idsection: string) {
+
+  }
+
+
+}
---
+++ src/app/store/store.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store',
+  templateUrl: './store.component.html',
+  styleUrls: ['./store.component.css']
+})
+export class StoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  open_direct_sell() {
+
+  }
+
+  open_prestashop() {
+
+  }
+}
